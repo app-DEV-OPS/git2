f866440511dcab02cdb4256b710c46c7cc27f250
adarsh <adarsh@WF.com>
2021-01-10 13:05:10 +0530

    Hello1.java updated

--- Hello1.java
+++ Hello1.java
@@ -1,5 +1,9 @@
 class Hello1{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     
+	    ystem.out.println("Hello1 Java");  
+    System.out.println("Hello2 Java");
+    System.out.println("Hello3 Java");
+    System.out.println("Hello4 Java");
     }  
 }  
