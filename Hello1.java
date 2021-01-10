636f12505c67f3bfa02e8d502edbcf38686f4501
Adhi2019 <59007036+Adhi2019@users.noreply.github.com>
2021-01-10 15:32:26 +0530

    Update Hello1.java

--- Hello1.java
+++ Hello1.java
@@ -1,7 +1,7 @@
 class Hello1{  
     public static void main(String args[]){  
      
-   system.out.println("Hello1 Java");  
+   System.out.println("Hello1 Java");  
     System.out.println("Hello2 Java");
     System.out.println("Hello3 Java");
     System.out.println("Hello4 Java");
